1cbfea8c38ac99fbb99ddb61efa083372a08d854
Aldwyn Cabarrubias <aldwyn.up@gmail.com>
2015-10-02 15:17:48 +0800

    added filter pipeline for item uniqueness

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # zalora-scraper
 A Scrapy-built spider that crawls throughout all the current items on Zalora.com
+
+Command: `scrapy crawl zalora -o items.json`
--- zalorascraper/pipelines.py
+++ zalorascraper/pipelines.py
@@ -1,11 +1,14 @@
-# -*- coding: utf-8 -*-
-
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from scrapy.exceptions import DropItem
 
 
-class ZalorascraperPipeline(object):
-    def process_item(self, item, spider):
-        return item
+class DuplicatesPipeline(object):
+
+	def __init__(self):
+		self.ids_seen = {}
+
+	def process_item(self, item, spider):
+		if item['id_catalog_config'] in self.ids_seen:
+			raise DropItem('Duplicate item found: %s' % item)
+		else:
+			self.ids_seen[item['id_catalog_config']] = None
+			return item
